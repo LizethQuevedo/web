9b00ef9c222705c8afcd92eb95200867c21f2922
LizethQuevedo <jlquena25@gmail.com>
2025-05-14 19:33:35 -0500

    Aprendimos a leer achivos con panda

---
+++ sesion 7/main.py
@@ -0,0 +1,12 @@
+# escribe esta linea de codigo
+print("bienvenidos al entrenamiento con python, vamos a disfrutar al maximo esta sesion")
+
+"""
+    descuento de una compra
+    si la compra es de $1000, obtienes un descuento de 20%
+    pide el monto de la compra y muestra el descuento final
+    """
+
+#pedir datos por teclado al usuario int (entero) float (decimal) str (cadena de caractares) bool (bolenos unos o ceros)
+
+compra = float(input("digite el valor de su compra: "))
